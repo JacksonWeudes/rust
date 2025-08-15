4e56f5ce74131a2aa54586bc6335e09b152e654a
JacksonWeudes <weudes2024jackson@gmail.com>
2025-08-15 16:01:58 -0300

    side-menu concluido

---
+++ index.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector(".mobile-menu-btn")
+const exitBtn = document.querySelector(".exit-btn")
+const sideMenu = document.querySelector(".side-menu")
+
+menuBtn.addEventListener("click", () => {
+    sideMenu.classList.add("active")
+})
+
+exitBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("active")
+})
