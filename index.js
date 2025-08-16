097c66f10690be74ad86ab363d50035bcadbb3f3
JacksonWeudes <weudes2024jackson@gmail.com>
2025-08-16 17:42:35 -0300

    erro no menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// menu lateral
 const menuBtn = document.querySelector(".mobile-menu-btn")
 const exitBtn = document.querySelector(".exit-btn")
 const sideMenu = document.querySelector(".side-menu")
